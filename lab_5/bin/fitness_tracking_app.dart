4bd01cd2d3707582f14e9ed66163484bd906fd9b
khaled <khaled1422_8@gmail.com>
2022-11-14 01:50:27 +0300

    gg

---
+++ lab_5/bin/fitness_tracking_app.dart
@@ -0,0 +1,11 @@
+
+//challenges list
+List<String> walking = ["5k", "speed:5k/h"];
+List<String> running = ["2k", "speed:10k/h"];
+List<List>? activity = [walking, running];
+var day = 3;
+List<String> challenge=[];
+
+
+//map fitness
+Map<String,double>level={"Easy":6.0 ,"Medium":8.0,"Fast":10.0};
